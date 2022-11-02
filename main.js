60649dcdd3129ced384f4fc35fe07c6e95fc42ba
Cecilie Maria Hernig <ceci7542@edu.easj.dk>
2022-11-02 16:18:21 +0100

    Add to cart method and hoverover methos for color

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
     data: function(){
         return {
+            cart: 0,
             product: 'Socks',
             // productDescription: 'You can choose a color for your',
             image: './assets/images/socks_blue.jpg',
@@ -8,8 +9,17 @@ const app = Vue.createApp({
             onSale: true,
             details: ['50% cotton', '30% wool', '20% poluester'],
             sizes: ['S', 'M', 'L'],
-            variants: [ {id: 2234, color: 'blue'}, {id: 2235, color: 'green'}]
+            variants: [ {id: 2234, color: 'blue', image: './assets/images/socks_blue.jpg'}, 
+                        {id: 2235, color: 'green', image: './assets/images/socks_green.jpg'}]
             // url: 'https://www.zealand.dk/'
+        }
+    },
+    methods: {
+        addToCart(){
+            this.cart += 1
+        },
+        updateImage(variantImage){
+            this.image = variantImage
         }
     }
 })
