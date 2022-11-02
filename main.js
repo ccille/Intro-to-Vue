1cabf58a84ded94b3459a6e01a22513d167c8e02
Cecilie Maria Hernig <ceci7542@edu.easj.dk>
2022-11-02 16:06:03 +0100

    List

--- main.js
+++ main.js
@@ -4,7 +4,12 @@ const app = Vue.createApp({
             product: 'Socks',
             // productDescription: 'You can choose a color for your',
             image: './assets/images/socks_blue.jpg',
-            url: 'https://www.zealand.dk/'
+            inventory: 8,
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% poluester'],
+            sizes: ['S', 'M', 'L'],
+            variants: [ {id: 2234, color: 'blue'}, {id: 2235, color: 'green'}]
+            // url: 'https://www.zealand.dk/'
         }
     }
 })
