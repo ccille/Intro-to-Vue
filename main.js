d461789846679c16b3352d1c873a60fd80929a3f
Cecilie Maria Hernig <ceci7542@edu.easj.dk>
2022-11-02 15:24:33 +0100

    Display picture and attribute bind

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data: function(){
         return {
             product: 'Socks',
-            productDescription: 'You can choose a color for your socks!'
+            // productDescription: 'You can choose a color for your',
+            image: './assets/images/socks_blue.jpg',
+            url: 'https://www.zealand.dk/'
         }
     }
 })
